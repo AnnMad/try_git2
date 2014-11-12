0d0827c73acf303186245e4c2bf98633ec310934
tygryss <protecttsoul@gmail.com>
2014-11-12 16:03:06 +0100

    Mnie też! Mnie też!

---
+++ src/ButEnterprises.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author LAB702-14
+ */
+public class ButEnterprises {
+
+public static void main(String[] args){ 
+
+System.out.println("IT'S A FINAL COUNTDOWN!");
+
+}
+
+}
+
